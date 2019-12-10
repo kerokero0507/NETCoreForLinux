cf407390dec3d63ed37e197475531cda72fa35dc
kerokero0507 <shohhei_mizuguchi@jp.ricoh.com>
2019-12-10 10:03:52 +0900

    [add]191210 //Hello World //.NET Core アプリのLinux駆動テスト用

---
+++ test_n1/test_n1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace test_n1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("私が作りました！");
+        }
+    }
+}
